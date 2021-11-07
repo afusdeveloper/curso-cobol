216ab4f3699ab87d87cd9c8f98dd24be748552c6
[afusdeveloper] <[aymara.fusaro@gmail.com]>
2021-11-07 19:42:29 -0300

    ...

---
+++ 009-IF-ELSE/IFELSE.cbl
@@ -0,0 +1,21 @@
+       IDENTIFICATION DIVISION.                                      
+       PROGRAM-ID.                     IFELSE.                       
+       ENVIRONMENT DIVISION.                                         
+       DATA DIVISION.                                                
+                                                                     
+       WORKING-STORAGE SECTION.                                      
+       01  WSC-CONSTANTES.                                           
+          05 WSC-A                     PIC 9 VALUE 7.                
+          05 WSC-B                     PIC 9 VALUE 9.                
+                                                                     
+       PROCEDURE DIVISION.                                           
+           PERFORM 010-IF                                            
+           PERFORM 010-END.                                          
+                                                                     
+       010-IF.                                                       
+           IF WSC-A < WSC-B                                          
+              DISPLAY 'EL VALOR DE ' WSC-A ' ES MENOR QUE ' WSC-B    
+           ELSE                                                      
+              DISPLAY 'EL VALOR DE ' WSC-A ' ES MAYOR QUE ' WSC-B.   
+              STOP RUN.                                              
+       010-END. EXIT.
